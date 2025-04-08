2db6ac1fc5505c8c998537939dd42f31872d1c4f
DylanBro <dylan.bouraoui83@gmail.com>
2025-04-08 14:07:26 +0200

    feat(navbar): Navbar anonymous

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AnonymousComponent } from './layout/anonymous/anonymous.component';
+import { HomeComponent } from './anonymous/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AnonymousComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        title: 'MyRocket',
+      },
+    ],
+  },
+];
