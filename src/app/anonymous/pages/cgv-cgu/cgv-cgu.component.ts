300fe9ff656c0e4397fef4c89041dc673f0042b4
DylanBro <dylan.bouraoui83@gmail.com>
2025-05-05 10:32:07 +0200

    feat(page): cgv cgu

---
+++ src/app/anonymous/pages/cgv-cgu/cgv-cgu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cgv-cgu',
+  imports: [],
+  templateUrl: './cgv-cgu.component.html',
+})
+export class CgvCguComponent {
+
+}
