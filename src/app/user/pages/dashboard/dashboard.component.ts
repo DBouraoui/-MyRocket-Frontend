e222838e1db2b4e07232e1d8cef288a6681a8edd
DylanBro <dylan.bouraoui83@gmail.com>
2025-05-06 13:20:12 +0200

    feat(notification): Validation des notification (unitaire et grouper)

--- src/app/user/pages/dashboard/dashboard.component.ts
+++ src/app/user/pages/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { UserWebsitesDisplayComponent } from '../../components/user-websites-dis
 import { UserWebsitesInformationsComponent } from '../../components/user-websites-informations/user-websites-informations.component';
 import { UserWebsitesMaintenanceContractsComponent } from '../../components/user-websites-maintenance-contracts/user-websites-maintenance-contracts.component';
 import { UserInformationsSettingsComponent } from '../../components/user-informations-settings/user-informations-settings.component';
-import { ButtonDirective } from 'primeng/button';
+import { Button, ButtonDirective } from 'primeng/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -13,7 +13,37 @@ import { RouterLink } from '@angular/router';
     UserWebsitesMaintenanceContractsComponent,
     UserInformationsSettingsComponent,
     RouterLink,
+    Button,
   ],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+
+  /**
+   * Télécharge le contrat de maintenance
+   */
+  downloadMaintenanceContract() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'Contrat-de-maintenance.pdf');
+    link.setAttribute('download', 'Contrat-de-maintenance.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  /**
+   * Télécharge le contrat de création de site web
+   */
+  downloadWebsiteContract() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'Contrat-creation.pdf');
+    link.setAttribute('download', 'Contrat-creation.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+
+}
